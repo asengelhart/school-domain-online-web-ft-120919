dbf3f5b3f75e13e8131c5964ff49d2f3063f0182
Alex Engelhart <asengelhart@yahoo.com>
2019-12-18 17:20:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,7 @@ class School
   end 
   def add_student(student_name, grade_num)
     @roster[grade_num] = [] if !@roster.include?(grade_num)
-    @roster[]
+    @roster[grade_num] << student_name if !@roster[grade_num].include?(student_name)
     
   end 
 end 
