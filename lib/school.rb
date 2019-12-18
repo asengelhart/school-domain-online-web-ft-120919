57893a916b11f08429d9226174495c68bdb5c2ee
Alex Engelhart <asengelhart@yahoo.com>
2019-12-18 17:26:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 class School
   attr_accessor :school_name, :roster
   def initialize(school_name)
-    @school_name = name 
+    @school_name = school_name 
     @roster = {}
   end 
   def add_student(student_name, grade_num)
